7667da0f76bb76036ab782255b38bfc862856947
Hannah Melendy <hrmelendy@gmail.com>
2019-05-12 11:00:56 -0700

    add Gratitude and Affirmation text boxes, submit button, and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
   $("form#greatful-1").submit(function(event) {
-    var someInput = $("input#some-input").val();
+    console.log("I'm here");
+    var someInput1 = $("input#gratitude-1").val();
+    console.log(someInput1);
+    event.preventDefault();
+  });
 
+  $("form#greatful-2").submit(function(event) {
+    console.log("I'm at a cafe");
+    var someInput2 = $("input#gratitude-2").val();
+    console.log(someInput2);
     event.preventDefault();
   });
 });

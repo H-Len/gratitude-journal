cddee6dee57b5c50df5c4418d3be77b3ba8b4f7e
Hannah Melendy <hrmelendy@gmail.com>
2019-05-21 22:19:09 -0700

    prepend time to gratitude submission

--- js/scripts.js
+++ js/scripts.js
@@ -71,7 +71,7 @@ $(document).ready(function() {
     var someInput1 = $("input#gratitude-1").val();
     console.log(someInput1);
     event.preventDefault();
-    $('li.question1Answer').append(someInput1 + '<br>');
+    $('li.question1Answer').append(timeStamp + " " + someInput1 + '<br>');
   });
 
 
